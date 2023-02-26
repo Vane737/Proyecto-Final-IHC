078a1a0de63d712b1d89a7b05c3a117d8746b8f9
Diana Montano <dianaerika.montano16@gmail.com>
2023-02-25 21:01:56 -0400

    Componente Check implementado

---
+++ src/components/Check/Check.js
@@ -0,0 +1,7 @@
+export default function Check() {
+  return (
+    <div class="form-check">
+      <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+    </div>
+  );
+}
