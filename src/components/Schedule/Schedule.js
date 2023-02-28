b6bf7015e1ed51e30b795d75763fd9ec0d0945c7
Vane737 <77525733+Vane737@users.noreply.github.com>
2023-02-28 15:59:37 -0600

    modificaciones en componentes

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -4,17 +4,28 @@ import ScheduleHour from './ScheduleHour';
 import ScheduleItem from './ScheduleItem';
 
 export default function Schedule({ arrMaterias, arrHorarios }) {
+
+    const addMins = (() => {
+        
+    })
+
   return (
     <div className=" container-schedule">
       {arrMaterias.map((materia, index) => {
         return (
           <>
+          
             {arrHorarios.map((horario, index) => {
               return (
                 <>
                   {materia.id === horario.idmat ? (
-                      <ScheduleItem materia={materia} horario={horario}/>
-                  ) : (
+                    <>
+                        <ScheduleItem materia={materia} horario={horario}/>
+                        {
+                            
+                        } 
+                    </>
+                      ) : (
                     ''
                   )}
                 </>
